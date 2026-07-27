59c6915d8a4bdf02333c4a16ec4603d724326e48
anywallsocket <98923858+anywallsocket@users.noreply.github.com>
2026-07-27 14:40:06 -0400

    Delete mc directory

--- mc/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Monte Carlo."""
-
-from ase.mc.logger import MCLogger
-from ase.mc.mc import MonteCarlo
-from ase.mc.moveset import Moveset
-from ase.mc.ensembles import NVT, NPT, BVT
-
-__all__ = ["MCLogger", "MonteCarlo", "Moveset", "NVT", "NPT"]
